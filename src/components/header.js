03c464163eec3dac583a437c453c9adb6431f2d3
ssthil <ssthil@gmail.com>
2018-01-17 16:46:20 +0800

    logo added

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Logo from '../images/ton.jpg'
 class Header extends Component {
     
     render() {
         return(
             <div className="jumbotron">
-                <h3 className="display-3 title">{this.props.title}</h3>
+                {/* <h3 className="display-3 title">{this.props.title}</h3> */}
+                <div className="logoBg"><img src={Logo} height="80" alt="TON" /></div>
                 <p className="lead description">{this.props.description}</p>
             </div>
         )
