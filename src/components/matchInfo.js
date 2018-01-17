d529ccfc72540a6861d918a6ea43c2f3b6cb82fa
ssthil <ssthil@gmail.com>
2018-01-17 11:22:42 +0800

    result updates

--- src/components/matchInfo.js
+++ src/components/matchInfo.js
@@ -18,7 +18,7 @@ const styles = {
                     this.props.info.map((match, i) => 
                         <div className="col-lg-6 col-sm-12 col-sm-12" key={i}>
                             <div className="list-group">
-                                <a href="" className={"list-group-item list-group-item-action" + ( match.inprogress ==='yes' ? ' animated-brd':'')} style={{borderTopColor: (match.result === 'won') ? match.wonColor : match.color}} >
+                                <a href="" className={"list-group-item list-group-item-action" + ( match.inprogress ==='yes' ? ' animated-brd':'')} style={{borderTopColor: (match.result === 'won' || match.result === 'lose') ? match.resultColor : match.color}} >
                                     <div className="justify-content-between ">
                                         <LeftSection timeInfo = {match}/>
                                         <RightSection teamInfo= {match}/>
