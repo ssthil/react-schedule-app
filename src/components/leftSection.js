be17c141a4b90c0536b9828c4d1d061cd58b37a4
ssthil <ssthil@gmail.com>
2018-05-21 17:27:01 +0800

    MODIFIED code to reduce the line of code

--- src/components/leftSection.js
+++ src/components/leftSection.js
@@ -3,32 +3,15 @@ import Images from '../images/images'
 
 class LeftSection extends Component {
 
-  won = () => {
-    return Images.won
-  }
-
-  lose = () => {
-    return Images.lose
-  }
-
-  tied = () => {
-    return Images.tied
-  }
-
   resultContent = (result) => {
     let actualContent;
     switch (result) {
       case 'won':
-
-        return actualContent = <img src={this.won()} alt="won" height="50" />
-
+        return actualContent = <img src={Images.won} alt="won" height="50" />
       case 'lose':
-
-        return actualContent = <img src={this.lose()} alt="lose" height="50" />
-
+        return actualContent = <img src={Images.lose} alt="lose" height="50" />
       case 'tied':
-        return actualContent = <img src={this.tied()} alt="tied" height="50" />
-
+        return actualContent = <img src={Images.tied} alt="tied" height="50" />
       default:
         return actualContent = <h5 className="text-bold">{this.props.timeInfo.time}</h5>
     } 
@@ -39,7 +22,6 @@ class LeftSection extends Component {
   render() {
     const ifMatchWon = this.props.timeInfo.result;
     return (
-
       <div className="left-content text-center">
         {this.resultContent(ifMatchWon)}
       </div>
